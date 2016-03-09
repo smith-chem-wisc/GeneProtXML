90e8b74d9b14ee197fa1f7ba011f9028b243cc02
acesnik <anthony.cesnik@gmail.com>
2016-03-09 13:21:47 -0600

    Implemented changes in proteogenomicdbgenerator into samplespecificdbgenerator to create the new program geneprotxml. This program should now have all the bug fixes and improvements that were implemented in samplespecificdbgenerator.

---
+++ geneprotxml.py
@@ -0,0 +1,152 @@
+# main program: samplespecificdbgenerator.py
+
+__author__ = "anthony"
+__date__ = "$Oct 27, 2015 2:41:36 PM$"
+
+import sys
+import os.path
+import optparse
+import refparse
+import novelsplices
+import variantcalls
+from lxml import etree as et
+
+HTML_NS = "http://uniprot.org/uniprot"
+XSI_NS = "http://www.w3.org/2001/XMLSchema-instance"
+NAMESPACE_MAP = {None:HTML_NS, "xsi":XSI_NS}
+UP = '{'+HTML_NS+'}'
+    
+def __main__():
+    #Parse Command Line
+    parser = optparse.OptionParser()
+      #I/O
+    parser.add_option( '-x', '--reference_xml', dest='reference_xml', help='Reference protein UniProt-XML file. Sequence variant peptide entries are appended to this database to generate the ouptut UniProt-XML protein database.' )
+    parser.add_option( '-p', '--protein_fasta', dest='protein_fasta', help='Reference protein FASTA file. Used to generate SAV peptide entries. If no UniProt-XML is specified, SAV and NSJ entries will be appended to this database to generate an output database. By default, this output will be a UniProt-XML protein database without PTM annotations. If --output-fasta is selected, the output will be a protein FASTA.')
+    parser.add_option( '-g', '--gene_model', dest='gene_model', default=None, help='GTF gene model file. Used to annotate NSJ peptide entries.')
+    parser.add_option( '-v', '--snpeff_vcf', dest='snpeff_vcf', help='SnpEff VCF file with HGVS annotations (else read from stdin).' )
+    parser.add_option( '-b', '--splice_bed', dest='splice_bed', help='BED file (tophat junctions.bed) with sequence column added.' )
+    parser.add_option( '-o', '--output', dest='output', help='Output file path. Outputs UniProt-XML format unless --output-fasta is selected.' )
+    parser.add_option( '-z', '--output_fasta', dest='output_fasta', action='store_true', default=False, help='Output a FASTA-format database. Place path for output file after the --output flag.')
+      #Peptide sequence construction
+    parser.add_option( '-l', '--leading_aa_num', dest='leading_aa_num', type='int', default=33, help='Leading number of AAs to output for SAV peptides. Default: 33.' )
+    parser.add_option( '-t', '--trailing_aa_num', dest='trailing_aa_num', type='int', default=33, help='Trailing number of AAs to output for SAV peptides. Default: 33.' )
+      #Filtering parameters
+    parser.add_option( '-D', '--nsj_depth_cutoff', dest='nsj_depth_cutoff', type='int', default=0, help='Keep only NSJs found with above this depth (BED score field). Default: 0.' )
+    parser.add_option( '-E', '--snv_depth_cutoff', dest='snv_depth_cutoff', type='int', default=0, help='Keep only SNVs found with above this depth (DP=# field). Default: 0.' )
+    parser.add_option( '-M', '--minimum_length', dest='minimum_length', type='int', default=0, help='Keep only sequence variant peptides with greater than or equal to this length. Default: 0.' )
+      #Simple entry
+    parser.add_option( '-Q', '--bed_score_name', dest='bed_score_name', default="depth", help='Include in the NSJ ID line score_name:score. Default: "depth."'  )
+    parser.add_option( '-R', '--reference', dest='reference', default="None", help='Genome Reference Name for NSJ ID location. Automatically pulled from genome_build header in GTF if present.'  )
+    (options, args) = parser.parse_args()
+    
+    ##INPUTS##
+    #Protein FASTA
+    try:
+        protein_fasta = os.path.abspath(options.protein_fasta)
+        protein_fasta = open(protein_fasta, 'r')
+        protein_fasta = refparse.read_protein_fasta(protein_fasta)
+    except Exception, e:
+        print >> sys.stderr, "failed: %s" % e
+        exit(2)
+    
+    #Reference XML/FASTA
+    ensembl, uniprot = None, None
+    if options.reference_xml != None:
+        try:
+            refXml = os.path.abspath(options.reference_xml)
+            refXml = open(refXml, 'r')
+            p = et.XMLParser(remove_blank_text=True) #required for pretty additions
+            uniprot = et.parse(refXml, p)
+            uniprot_root = uniprot.getroot()
+            for entry in uniprot_root:
+                refparse.condense_xml_entry(entry)
+        except Exception, e:
+            print >> sys.stderr, "Parsing and/or condensing reference xml failed: %s" % e
+            exit(2)
+    else:
+        print >> sys.stderr, "failed: no UniProt reference protein database specified"
+
+    if options.protein_fasta != None:
+        try:
+            refFasta = os.path.abspath(options.protein_fasta)
+            refFasta = open(refFasta, 'r')
+            ensembl_root = et.Element(UP+'uniprot', nsmap=NAMESPACE_MAP)
+            ensembl = et.ElementTree(ensembl_root)
+            refparse.read_fasta_to_xml(ensembl_root, refFasta)
+        except Exception, e:
+            print >> sys.stderr, "Parsing reference fasta failed: %s" %e
+            exit(2)
+    else:
+        print >> sys.stderr, "failed: no Ensembl reference protein database specified"
+
+    ##OUTPUT##
+    outFile = None
+    if options.output == None: outFile = sys.stdout
+    else:
+        try:
+            outFile = os.path.abspath(options.output)
+            outFile = open(outFile, 'w')
+        except Exception, e:
+            print >> sys.stderr, "Opening outfile failed: %s" % e
+            exit(3)
+            
+    #Process gene model
+    try:
+        geneModelFile = os.path.abspath(options.gene_model)
+        linect = sum(1 for line in open(geneModelFile))
+        geneModelFile = open(geneModelFile, 'r')
+        geneModel = refparse.GeneModel()
+        for i, gtf_line in enumerate(geneModelFile):
+            if i % 20000 == 0: print "gene_model line " + str(i) + " of " + str(linect)
+            if gtf_line.startswith('#') and  gtf_line.find('genome_build') >= 0: options.reference = gtf_line.split()[1]
+            elif gtf_line.startswith('#'): continue
+            else: geneModel.new_entry(gtf_line)
+    except Exception, e:
+        print >> sys.stderr, "Parsing gene model failed: %s" % e
+        exit(2)
+    
+    #Process VCF
+    try:
+        snpeff_vcf = os.path.abspath(options.snpeff_vcf)
+        linect = sum(1 for line in open(snpeff_vcf))
+        snpeff_vcf = open(snpeff_vcf, 'r')
+        for i, vcf_line in enumerate(snpeff_vcf):
+            if i % 1000 == 0: print "snpeff_vcf line " + str(i) + " of " + str(linect)
+            variantcalls.parse_vcf_line(ensembl_root, vcf_line, protein_fasta, options.snv_depth_cutoff, options.minimum_length, options.leading_aa_num, options.trailing_aa_num)
+        snpeff_vcf.close()
+    except Exception, e:
+        print >> sys.stderr, "VCF processing failed: %s" % e
+        exit(1)
+    
+    #Process Splice BED
+    try:
+        splice_bed = os.path.abspath(options.splice_bed)
+        linect = sum(1 for line in open(splice_bed))
+        splice_bed = open(splice_bed, 'r')
+        for i, bed_line in enumerate(splice_bed):
+            if i % 2000 == 0: print "splice_bed line " + str(i) + " of " + str(linect)
+            if bed_line.startswith('track'): continue
+            novelsplices.translate_bed_line(ensembl_root, geneModel, bed_line, options.nsj_depth_cutoff, options.minimum_length, options.reference, options.bed_score_name)
+        splice_bed.close()
+    except Exception, e:
+        print >> sys.stderr, "Splice BED processing failed: %s" % e
+        exit(2)
+
+    #Enter PTM information from uniprot into the EnsemblXML
+    refparse.unify_xml(ensembl, uniprot)
+
+    #Write the sample specific database to outfile
+    if not options.output_fasta: ensembl.write(outFile, pretty_print=True)
+    else:
+        entryct = len(ensembl_root)
+        if outFile != None:
+            for i, entry in enumerate(ensembl_root):
+                if i % 1000 == 0: print "writing entry " + str(i) + " of " + str(entryct)
+                entry = refparse.xml_to_fasta(entry)
+                if entry == None: continue
+                else: outFile.write(entry[0] + '\n' + entry[1] + '\n')
+            outFile.close()
+        else: print >> sys.stderr, "Writing to fasta failed: no out-file found"
+
+if __name__ == "__main__" : __main__()
+
---
+++ refparse.py
@@ -0,0 +1,270 @@
+# module name: refparse
+# main program: samplespecificdbgenerator
+
+__author__ = "anthony"
+__date__ = "$Oct 27, 2015 2:54:44 PM$"
+
+import variantcalls
+from lxml import etree as et
+
+HTML_NS = "http://uniprot.org/uniprot"
+XSI_NS = "http://www.w3.org/2001/XMLSchema-instance"
+NAMESPACE_MAP = {None : HTML_NS, "xsi" : XSI_NS}
+UP = '{'+HTML_NS+'}'
+
+def aa_abbrev_dict():
+## dictionary for Amino Acid Abbreviations
+    aa_abbrev_dict = dict()
+    aa_abbrev_dict['Phe'] = 'F'
+    aa_abbrev_dict['Leu'] = 'L'
+    aa_abbrev_dict['Ser'] = 'S'
+    aa_abbrev_dict['Tyr'] = 'Y'
+    aa_abbrev_dict['Cys'] = 'C'
+    aa_abbrev_dict['Trp'] = 'W'
+    aa_abbrev_dict['Pro'] = 'P'
+    aa_abbrev_dict['His'] = 'H'
+    aa_abbrev_dict['Gln'] = 'Q'
+    aa_abbrev_dict['Arg'] = 'R'
+    aa_abbrev_dict['Ile'] = 'I'
+    aa_abbrev_dict['Met'] = 'M'
+    aa_abbrev_dict['Thr'] = 'T'
+    aa_abbrev_dict['Asn'] = 'N'
+    aa_abbrev_dict['Lys'] = 'K'
+    aa_abbrev_dict['Val'] = 'V'
+    aa_abbrev_dict['Ala'] = 'A'
+    aa_abbrev_dict['Asp'] = 'D'
+    aa_abbrev_dict['Glu'] = 'E'
+    aa_abbrev_dict['Gly'] = 'G'
+    return aa_abbrev_dict
+
+def condense_xml_entry(entry):
+    for element in entry:
+        if element.tag not in [UP+'protein',UP+'accession',UP+'name',UP+'gene',UP+'organism',UP+'proteinExistence',UP+'depth',UP+'sequence',UP+'feature',UP+'dbReference']:
+            entry.remove(element)
+        elif element.get('type') != 'modified residue' and element.tag == UP+'feature': entry.remove(element)
+        elif element.get('type') != 'Ensembl' and element.tag == UP+'dbReference': entry.remove(element)
+        elif element.tag == UP+'organism':
+            for field in element:
+                if field.tag != UP+'name': element.remove(field)
+        elif element.tag == UP+'protein':
+            for name in element:
+                if name.tag != UP+'recommendedName': element.remove(name)
+        else: continue
+
+
+def add_unified(root, newId, rootIndex, acc, seqtype, chromosome, biotypes, geneId, transcriptId, seq):
+    if newId:
+        entry = et.SubElement(root, UP+'entry', dataset="Ensembl")
+        accession = et.SubElement(entry, UP+'accession')
+        accession.text = str(newId)
+    else: entry = root[rootIndex]
+
+    accession = et.Element(UP+'accession')
+    accession.text = acc
+    a = entry.findall(UP+'accession')
+    if len(a) > 0: a[-1].addnext(et.Element(UP+'accession'))
+    else: entry.append(accession)
+
+    name = et.Element(UP+'name')
+    name.text = seqtype
+    n = entry.findall(UP+'name')
+    if len(n) > 0: n[-1].addnext(et.Element(UP+'name'))
+    else: entry.append(name)
+
+    if newId:
+        fullName = et.SubElement(et.SubElement(et.SubElement(entry, UP+'protein'), UP+'recommendedName'), UP+'fullName')
+        fullName.text = 'referenceProtein' + str(newId)
+        gene = et.SubElement(entry, UP+'gene')
+    else:
+        gene = et.Element(UP+'gene')
+        entry.findall(UP+'gene')[-1].addnext(gene)
+    geneName1 = et.SubElement(gene, UP+'name', type="coords")
+    geneName1.text = chromosome
+    geneName2 = et.SubElement(gene, UP+'name', type="primary")
+    geneName2.text = geneId
+    gene_biotype = et.SubElement(gene, UP+'gene_biotype')
+    gene_biotype.text = biotypes.split(' ')[0]
+    transcript = et.SubElement(gene, UP+'transcript', type="primary")
+    transcript.text = transcriptId
+    transcript_biotype = et.SubElement(gene, UP+'transcript_biotype')
+    transcript_biotype.text = biotypes.split(' ')[1]
+
+    if newId:
+        organism = et.SubElement(entry, UP+'organism')
+        organismName1 = et.SubElement(organism, UP+'name', type="scientific")
+        organismName2 = et.SubElement(organism, UP+'name', type="common")
+        organismName1.text = "Homo sapiens"
+        organismName2.text = "Human"
+        proteinExist = et.SubElement(entry, UP+'proteinExistence', type="evidence at transcript level")
+        sequence = et.SubElement(entry, UP+'sequence', version="1", fragment="single")
+        sequence.text = seq
+
+def ensembl_entry(uniqSeqs, root, acc, seqtype, chromosome, biotypes, geneId, transcriptId, seq):
+    found = False
+    for i, s in enumerate(uniqSeqs):
+        if seq == s:
+            found = True
+            add_unified(root, '', i, acc, seqtype, chromosome, biotypes, geneId, transcriptId, seq)
+            break
+    if not found:
+        uniqSeqs.append(seq)
+        add_unified(root, str(len(uniqSeqs)), -1, acc, seqtype, chromosome, biotypes, geneId, transcriptId, seq)
+
+def unify_xml(ensembl, uniprot):
+    #TODO: this might be more efficient if I got all of the nodes up front and then just got the parents...
+    ensembl_root, uniprot_root = ensembl.getroot(), uniprot.getroot()
+    uniprot_root.remove(uniprot_root.find(UP+'copyright'))
+    ensembl_seqs = [x.find(UP+'sequence').text.replace('\n','').replace('\r','') for x in ensembl_root] #lists all sequences without linebreaks
+    uniprot_seqs = [x.find(UP+'sequence').text.replace('\n','').replace('\r','') for x in uniprot_root] #lists all sequences without linebreaks
+
+    common_seqs = list(set(ensembl_seqs) & set(uniprot_seqs))
+    uniprot_only = list(set(uniprot_seqs) - set(common_seqs))
+    ensembl_only = list(set(ensembl_seqs) - set(common_seqs))
+
+    for s in common_seqs:
+        e_entry, u_entry = None, None
+        for i, t in enumerate(ensembl_seqs):
+            if s == t:
+                e_entry = ensembl_root[i]
+                break
+        for i, t in enumerate(uniprot_seqs):
+            if s == t:
+                u_entry = uniprot_root[i]
+                break
+        mods = u_entry.findall(UP+'feature')
+        seq = e_entry.find(UP+'sequence')
+        for mod in mods:
+            seq.addprevious(mod)
+    return ensembl
+
+def get_protein_fasta_seq(id, protein_fasta):
+    i = 0
+    for item in protein_fasta[0]:
+        if item.find(id) >= 0: return protein_fasta[0][i], protein_fasta[1][i]
+        i += 1
+    return None, None
+
+#Get header and seq from protein fasta using transcript or protein ensembl accession
+def read_fasta_to_xml(root, refFasta):
+    seq = ""
+    line = refFasta.readline().strip()
+    while line != "":
+        if line.startswith(">"):
+            line = line.split()
+            acc, seqtype, chromosome = line[0][1:], line[1], line[2]
+            geneId, transcriptId, addedInfo = line[3].split(':')[1], line[4].split(':')[1], ' '.join(line[5:])
+            line = refFasta.readline().strip()
+            while not line.startswith(">"):
+                if line == "": break
+                seq += line
+                line = refFasta.readline().strip()
+            if seq.find('*') < 0: variantcalls.enter_seqvar(root, acc, seqtype, chromosome, addedInfo, '',  '', geneId, transcriptId, seq)
+            seq = ""
+          
+#Reads the headers and sequences of a fasta file into RAM
+def read_protein_fasta(protein_fasta):
+    proteinFasta = ([],[]) #headers, #sequences
+    line = protein_fasta.readline()
+    while line.startswith ('#'):
+        line = protein_fasta.readline()
+    sequence = ""
+    while line != "":
+        if line.startswith(">"):
+            proteinFasta[0].append(line)
+            line = protein_fasta.readline()
+            while not line.startswith(">"):
+                if line == "": break
+                sequence += line
+                line = protein_fasta.readline()
+            proteinFasta[1].append(sequence.replace('\n','').replace('\r',''))
+            sequence = ""
+    protein_fasta.close()
+    return proteinFasta
+
+#Returns information in an XML entry in fasta duplet: (header, sequence)
+def xml_to_fasta(entry):
+    header = ">"
+    if entry.tag == UP+'copyright': return None
+    if entry.get('dataset') == 'Ensembl':
+        accession, name, geneInfo, chromosome, geneId, transcriptId, addedInfo = None, None, None, None, None, None, None
+        accession = entry.find(UP+'accession').text
+        name = entry.find(UP + 'name').text
+        geneInfo = entry.find(UP+'gene')
+        if geneInfo != None: 
+            geneInfo.getiterator(UP+'name')
+            for item in geneInfo:
+                if item.get('type') == 'coords': chromosome = item.text
+                if item.get('type') == 'primary':
+                    geneId = 'gene:' + item.text
+                    transcriptId = 'transcript:' + item.find(UP+'transcript').text
+        addedInfo = entry.find(UP + 'protein').find(UP + 'recommendedName').find(UP + 'fullName').text
+        score = entry.find(UP + 'proteinExistence').find(UP + 'depth')
+        score = 'depth:' + score.get('reads') if score != None else ''
+        headerInfo = [x for x in [accession, name, chromosome, geneId, transcriptId, addedInfo, score] if x != None] #remove None elements
+        header += ' '.join(headerInfo)
+    else:
+        if entry.get('dataset') == 'Swiss-Prot': database = 'sp'
+        else: database = 'tr'
+        accession = entry.find(UP + 'accession').text
+        name = entry.find(UP + 'name').text
+        accession = '|'.join([database, accession, name])
+        organism = entry.find(UP + 'organism')
+        if organism != None: organism = 'OS=' + organism.find(UP+'name').text
+        geneName = entry.find(UP+ 'gene')
+        if geneName != None: geneName = 'GN=' + geneName.find(UP+'name').text
+        headerInfo = [x for x in [accession, organism, geneName] if x != None] 
+        header += ' '.join(headerInfo)
+    return(header.replace('\n','').replace('\r',''),  entry.find(UP+'sequence').text.replace('\n','').replace('\r',''))
+    
+###GENE MODEL STRUCTURE###
+class Annotation():
+    def __init__(self, name, biotype):
+        self.name = name
+        self.biotype = biotype
+            
+class Chromosome():   
+    def __init__(self, id, strand):
+        self.id = id
+        self.strand = strand
+        self.annotation_types = {}
+        self.ranges = []
+        
+    def update_last_range_index(self, i):
+        self.lastRangeIndex = i
+        
+    def identify_range(self, start, end):
+        for range in self.ranges:
+            chromStart, chromEnd, annotation = range
+            startInBounds = start <= chromEnd and start >= chromStart
+            endInBounds = end <= chromEnd and end >= chromStart
+            spanning = start < chromStart and end > chromEnd
+            if startInBounds or endInBounds or spanning: return (annotation.name, annotation.biotype)
+        return ('', 'novel')
+        
+    def enter_annotation(self, chromStart, chromEnd, name, biotype):
+        if (name, biotype) not in self.annotation_types: self.annotation_types[(name, biotype)] = Annotation(name, biotype)
+        self.ranges.append((chromStart, chromEnd, self.annotation_types[(name, biotype)]))
+        
+class GeneModel():
+    def __init__(self):
+        self.chromosomes = {}
+        
+    def get_chrom(self, id, strand):
+        if (id, strand) not in self.chromosomes: self.chromosomes[(id, strand)] = Chromosome(id, strand)
+        return self.chromosomes[(id, strand)]
+    
+    def identify_range(self, chrom, strand, start, stop):
+        return self.chromosomes[(chrom, strand)].identify_range(start, stop)
+    
+    def new_entry(self, line):
+        line = line.split('\t')
+        chromStart, chromEnd, chrom, strand = int(line[3]), int(line[4]), line[0], line[6]
+        attribute_list = line[8].split('; ')
+        attributes = {} #This is what's consistent across Ensembl gene models...
+        for item in attribute_list:
+            item = item.split(' ')
+            attributes[item[0]] = item[1][1:-1]
+        if 'transcript_biotype' not in attributes: attributes['transcript_biotype'] = line[1] #canonical gtf
+        
+        chromosome = self.get_chrom(chrom, strand)
+        if 'gene_name' in attributes and 'gene_biotype' in attributes: chromosome.enter_annotation(chromStart, chromEnd, attributes['gene_name'], attributes['gene_biotype'])
